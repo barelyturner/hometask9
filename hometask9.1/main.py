8a52a779f412de542e71870303fb5d57f986fa8a
wasilii <incredibleturner@gmail.com>
2023-08-15 23:04:57 +0300

    add

--- hometask9.1/main.py
+++ hometask9.1/main.py
@@ -1,8 +1,9 @@
-with open("month.txt", "r") as file:
-    content = file.readlines()
-    content.reverse()
-with open("month_reversed.txt", "a") as f:
-    content[0] += "\n"
-    content[-1] = content[-1].replace("\n", "")
-    for i in content:
-        f.write(str(i))
+# with open("month.txt", "r") as file:
+#     content = file.readlines()
+#     content.reverse()
+# with open("month_reversed.txt", "a") as f:
+#     content[0] += "\n"
+#     content[-1] = content[-1].replace("\n", "")
+#     for i in content:
+#         f.write(str(i))
+
