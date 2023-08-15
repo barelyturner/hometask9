253adf6a6fc69f3d0b6b14f5025d3308c0fa6761
wasilii <incredibleturner@gmail.com>
2023-08-15 22:37:54 +0300

    edited

--- hometask9.1/main.py
+++ hometask9.1/main.py
@@ -3,7 +3,7 @@ with open("month.txt", "r") as file:
     content.reverse()
 with open("month_reversed.txt", "a") as f:
     content[0] += "\n"
-    content[-1].replace("\n", "")
+    content[-1] = content[-1].replace("\n", "")
     for i in content:
         f.write(str(i))
 
